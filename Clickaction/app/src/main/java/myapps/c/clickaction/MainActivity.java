c0f2e71c541c315a2fa12a1b13bf1342fb31dde2
moltenmoto <k018c1399@it-neec.jp>
2020-07-14 22:51:37 +0900

    button action

---
+++ Clickaction/app/src/main/java/myapps/c/clickaction/MainActivity.java
@@ -0,0 +1,28 @@
+package myapps.c.clickaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("click");
+
+            }
+
+
+        });
+    }
+}
+
+
